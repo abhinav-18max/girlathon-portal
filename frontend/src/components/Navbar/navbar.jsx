c8f31ee76ac292577e0289f5310caa10e6775e31
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2024-02-18 16:50:00 +0530

    new

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -216,7 +216,15 @@ export default function Navbar() {
           </li>
         </ul>
       </section>
-      {(activeSection!='HERO' && topvis) &&<a href="#HERO" className="back-to-top" style={{ position:'fixed',bottom:'2rem',right:'1rem',zIndex:'100',border:'solid white .13rem',padding:'.3rem',display:'flex',justifyContent:'center',alignItems:'center' }}>TOP</a>}
+      {(activeSection!='HERO' && topvis) &&<a href="#HERO" className="back-to-top" style={{ position:'fixed',bottom:'2rem',right:'1rem',zIndex:'100'}}>
+      <button className="button-top">
+  <svg className="svgIcon" viewBox="0 0 384 512">
+    <path
+      d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"
+    ></path>
+  </svg>
+</button>
+     </a>}
     </div>
   );
 }
