dfe4d71a40cc89457af4be005e80be054c8255c0
BifinJ <jamesbifin@gmail.com>
2024-02-26 20:01:12 +0530

    media-one added

---
+++ frontend/src/components/commpart/media.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Media() {
+  return (
+    <section className="media-part p-5" style={{ height: "40dvh" }}>
+      <div>
+        <h1>Media Partner</h1>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: "80%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
+          gap: "25px",
+        }}
+      >
+        <div>
+          <div className="sponser media-card">
+            <a href="https://kalkitech.com/" className="card card1"></a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
